68af8177ac367c49de03ea2d213e387d18b3f951
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2023-02-09 21:43:43 +0200

    add responsive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(function () {
+  const menu_btn = document.querySelector('.hamburger');
+  const menu_header = document.querySelector('.menu__header');
+  menu_btn.addEventListener('click', function () {
+    menu_btn.classList.toggle('is-active');
+    menu_header.classList.toggle('is-active');
+  });
+
   const mixer = mixitup('.works');
 });
