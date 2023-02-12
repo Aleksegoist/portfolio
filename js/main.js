b866ad501bb3f2f74cb131e347f86b21a9bf3de2
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2023-02-12 19:23:54 +0200

    add

--- js/main.js
+++ js/main.js
@@ -1,5 +1,42 @@
 $(function () {
-  // const mixer = mixitup('.works');
+  const mixer = mixitup('.works');
+
+  $('.menu__header a').on('click', function (e) {
+    //отменяем стандартную обработку нажатия по ссылке
+    e.preventDefault();
+    //забираем идентификатор блока с атрибута href
+    var id = $(this).attr('href'),
+      //узнаем высоту от начала страницы до блока на который ссылается якорь
+      top = $(id).offset().top;
+    //анимируем переход на расстояние - top за 1500 мс
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() >= 200) {
+      $('.header__nav').addClass('sticky');
+    } else {
+      $('.header__nav').removeClass('sticky');
+    }
+  });
+
+  const headerHeight = $('.header__nav').outerHeight();
+
+  $('.menu__header-item a').on('click', function (e) {
+    e.preventDefault();
+
+    const scrollAnchor = $(this).attr('href');
+
+    let scrollPoint = $(scrollAnchor).offset().top - headerHeight;
+
+    $('html, body').animate(
+      {
+        scrollTop: scrollPoint,
+      },
+      500
+    );
+    return false;
+  });
 
   const burger = document.querySelector('.burger');
   const headerNav = document.querySelector('.header__nav');
